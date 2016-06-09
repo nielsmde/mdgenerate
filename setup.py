760f6c496989e28ece519486be7e8bcfffb48ee5
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-06-09 15:35:58 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from mdgenerate import __version__
+
+
+setup(
+    name='mdgenerate',
+    description='Collection of python utilities for generation of gromacs simulations.',
+    author_email='niels.mueller@physik.tu-darmstadt.de',
+    packages=['mdgenerate'],
+    version=__version__,
+    requires=['numpy', 'mdevaluate', 'yaml', 'jinja2'],
+)
